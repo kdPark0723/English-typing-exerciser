5fa49449b364e58fb130cecb87262a38ba9fb16a
Park ki-dong <kdpark0723@gmail.com>
2018-06-09 12:30:19 +0900

    Platform.h, main.c 통합, 긴글 연습 출력 수정

--- Main/main.c
+++ Main/main.c
@@ -1,8 +1,29 @@
 ﻿/**
-* include
-*/
+ * Platform 인식
+ */
 
-#include "Platform.h"
+#define _PLATFORM_UNDEFINED	0
+#define _PLATFORM_WIN32		1
+#define _PLATFORM_LINUX     2
+#define _PLATFORM_UNIX      3
+
+#if defined(_WIN32)
+#   define _PLATFORM_TYPE _PLATFORM_WIN32
+#   if  !(defined  _CRT_SECURE_NO_WARNINGS)
+#       define _CRT_SECURE_NO_WARNINGS
+#   endif
+#   if !(defined _SCL_SECURE_NO_WARNINGS)
+#       define _SCL_SECURE_NO_WARNINGS
+#   endif
+#elif defined(__linux__)
+#   define _PLATFORM_TYPE _PLATFORM_LINUX
+#elif defined(__unix__) || defined(__unix) || defined(unix) // all unices not caught above
+#   define _PLATFORM_TYPE _PLATFORM_UNIX
+#endif
+
+/**
+ * include
+ */
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -18,8 +39,8 @@ int _getch(void);
 #endif
 
 /**
-* define
-*/
+ * define
+ */
 
 #define TYPE_MENU 0
 #define TYPE_SEATPRACTICE 1
@@ -35,8 +56,8 @@ int _getch(void);
 #define INPUT_MAX 2000
 
 /**
-* 함수 prototypes
-*/
+ * 함수 prototypes
+ */
 
 int run(void);
 
@@ -79,8 +100,8 @@ char* get_resource(void);
 int long_sentence_divide_line(char*, char*, char*);
 
 /**
-* 외부 변수 선언
-*/
+ * 외부 변수 선언
+ */
 
 int is_running = 0;
 
@@ -124,12 +145,12 @@ char* resorce_long_sentence_practice[RESOURCE_SIZE_LONGSENTENCEPRACTICE] = { "I 
 char resorce_back_to_the_menu[] = "Enter키를 입력하시면 Menu로 이동합니다.";
 
 /**
-* 함수 정의
-*/
+ * 함수 정의
+ */
 
 /**
-* main: 메인 루프를 활성화해주고, 난수를 초기화하고, run함수를 호출합니다.
-*/
+ * main: 메인 루프를 활성화해주고, 난수를 초기화하고, run함수를 호출합니다.
+ */
 int main(void)
 {
     is_running = 1;
@@ -142,8 +163,8 @@ int main(void)
 
 
 /**
-* run: screen_clear, screen_draw, check_keyboard을 호출하는 메인 루프를 실행합니다.
-*/
+ * run: screen_clear, screen_draw, check_keyboard을 호출하는 메인 루프를 실행합니다.
+ */
 int run(void)
 {
     input_max = 1;
@@ -159,8 +180,8 @@ int run(void)
 }
 
 /**
-* check_keyboard: 키보드 입력을 받아 입력 값에 따라 다른 함수를 호출 해주고, 특수 문자가 아닌 경우 input_buffer에 넣어줍니다.
-*/
+ * check_keyboard: 키보드 입력을 받아 입력 값에 따라 다른 함수를 호출 해주고, 특수 문자가 아닌 경우 input_buffer에 넣어줍니다.
+ */
 int check_keyboard(void)
 {
     char ch;
@@ -177,9 +198,7 @@ int check_keyboard(void)
     }
     else if (ch == '\b')
     {
-        input_keyboard_backspace();
-
-        if (input_num > 0)
+        if (!input_keyboard_backspace() && input_num > 0)
         {
             input_num--;
             input_buffer[input_num] = 0;
@@ -201,8 +220,8 @@ int check_keyboard(void)
 }
 
 /**
-* screen_clear: 화면을 지워 줍니다.
-*/
+ * screen_clear: 화면을 지워 줍니다.
+ */
 int screen_clear(void)
 {
     printf("\n");
@@ -215,8 +234,8 @@ int screen_clear(void)
 }
 
 /**
-* screen_change: _type의 값으로 화면을 바꿔 줍니다.
-*/
+ * screen_change: _type의 값으로 화면을 바꿔 줍니다.
+ */
 int screen_change(int _type)
 {
 
@@ -227,7 +246,7 @@ int screen_change(int _type)
         return _type;
     }
     else if (_type < 0 || _type > 5)
-        return 0;
+        return -1;
 
     window_type = _type;
 
@@ -282,8 +301,8 @@ int screen_change(int _type)
 }
 
 /**
-* screen_draw: 화면을 window_type에 따라 그려주기 위해 각각 다른 함수를 호출해줍니다
-*/
+ * screen_draw: 화면을 window_type에 따라 그려주기 위해 각각 다른 함수를 호출해줍니다
+ */
 int screen_draw(void)
 {
     switch (window_type)
@@ -311,8 +330,8 @@ int screen_draw(void)
 }
 
 /**
-* input_keyboard: 키보드에 특수 문자가 아닌 다른 값이 입력 되었을 때 호출됩니다. 현재 입력된 값인 _input을 window_type에 각각 다른 함수에 넘겨줍니다.
-*/
+ * input_keyboard: 키보드에 특수 문자가 아닌 다른 값이 입력 되었을 때 호출됩니다. 현재 입력된 값인 _input을 window_type에 각각 다른 함수에 넘겨줍니다.
+ */
 int input_keyboard(char _input)
 {
     switch (window_type)
@@ -340,8 +359,8 @@ int input_keyboard(char _input)
 }
 
 /**
-* input_keyboard_enter: 키보드에 Enter키 또는 줄바꿈 문자가 입력 되었을 때 호출됩니다. window_type에 각각 다른 함수을 호출해줍니다.
-*/
+ * input_keyboard_enter: 키보드에 Enter키 또는 줄바꿈 문자가 입력 되었을 때 호출됩니다. window_type에 각각 다른 함수을 호출해줍니다.
+ */
 int input_keyboard_enter(void)
 {
     switch (window_type)
@@ -369,37 +388,39 @@ int input_keyboard_enter(void)
 }
 
 /**
-* input_keyboard_backspace: 키보드에 backspace키 또는 delete가 입력 되었을 때 호출됩니다. window_type에 각각 다른 함수을 호출해줍니다.
-*/
+ * input_keyboard_backspace: 키보드에 backspace키 또는 delete가 입력 되었을 때 호출됩니다. window_type에 각각 다른 함수을 호출해줍니다.
+ */
 int input_keyboard_backspace(void)
 {
+    int result = 0;
+
     switch (window_type)
     {
     case TYPE_MENU:
-        menu_input_keyboard_backspace();
+        result = menu_input_keyboard_backspace();
         break;
     case TYPE_SEATPRACTICE:
-        seat_practice_input_keyboard_backspace();
+        result = seat_practice_input_keyboard_backspace();
         break;
     case TYPE_WORDPRACTICE:
-        word_practice_input_keyboard_backspace();
+        result = word_practice_input_keyboard_backspace();
         break;
     case TYPE_SHORTSENTENCEPRACTICE:
-        short_sentence_practice_input_keyboard_backspace();
+        result = short_sentence_practice_input_keyboard_backspace();
         break;
     case TYPE_LONGSENTENCEPRACTICE:
-        long_sentence_practice_input_keyboard_backspace();
+        result = long_sentence_practice_input_keyboard_backspace();
         break;
     default:
         break;
     }
 
-    return 0;
+    return result;
 }
 
 /**
-* menu_draw: menu 화면을 그려줍니다.
-*/
+ * menu_draw: menu 화면을 그려줍니다.
+ */
 int menu_draw(void)
 {
     printf(">> 영문 타자 연습 프로그램 <<\n");
@@ -412,8 +433,8 @@ int menu_draw(void)
 }
 
 /**
-* seat_practice_draw: seat_practice 화면을 그려줍니다.
-*/
+ * seat_practice_draw: seat_practice 화면을 그려줍니다.
+ */
 int seat_practice_draw(void)
 {
     printf(">> 영문 타자 연습 프로그램 : 자리연습 <<\n");
@@ -426,8 +447,8 @@ int seat_practice_draw(void)
 }
 
 /**
-* word_practice_draw: word_practice 화면을 그려줍니다.
-*/
+ * word_practice_draw: word_practice 화면을 그려줍니다.
+ */
 int word_practice_draw(void)
 {
     printf(">> 영문 타자 연습 프로그램 : 낱말 연습 <<\n");
@@ -440,8 +461,8 @@ int word_practice_draw(void)
 }
 
 /**
-* short_sentence_practice_draw: short_sentence_practice 화면을 그려줍니다.
-*/
+ * short_sentence_practice_draw: short_sentence_practice 화면을 그려줍니다.
+ */
 int short_sentence_practice_draw(void)
 {
     printf(">> 영문 타자 연습 프로그램 : 짧은 글 연습 <<\n");
@@ -454,22 +475,22 @@ int short_sentence_practice_draw(void)
 }
 
 /**
-* long_sentence_practice_draw: long_sentence_practice 화면을 그려줍니다.
-*/
+ * long_sentence_practice_draw: long_sentence_practice 화면을 그려줍니다.
+ */
 int long_sentence_practice_draw(void)
 {
     printf(">>영문 타자 연습 프로그램 : 긴 글 연습<<\n");
     printf("정확도 : %d%%    현재타수 : %d\n\n", accuracy, current_typing_count);
     if (output_buffer)
-        printf("%s\n", output_buffer);
+        printf("%s\n\n", output_buffer);
     printf("%s", input_buffer);
 
     return 0;
 }
 
 /**
-* get_resource: window_type별로 다른 리소스를 찾아서 Return해 줍니다.
-*/
+ * get_resource: window_type별로 다른 리소스를 찾아서 Return해 줍니다.
+ */
 char* get_resource(void)
 {
     char* ch = 0;
@@ -498,16 +519,16 @@ char* get_resource(void)
 }
 
 /**
-* menu_input_keyboard: 키보드에 특수 문자가 아닌 다른 값이 입력 되었을 때 호출됩니다. 입력된 값이 _input에 넘겨집니다.
-*/
+ * menu_input_keyboard: 키보드에 특수 문자가 아닌 다른 값이 입력 되었을 때 호출됩니다. 입력된 값이 _input에 넘겨집니다.
+ */
 int menu_input_keyboard(char _input)
 {
     return 0;
 }
 
 /**
-* seat_practice_input_keyboard: 키보드에 특수 문자가 아닌 다른 값이 입력 되었을 때 호출됩니다. 입력된 값이 _input에 넘겨집니다.
-*/
+ * seat_practice_input_keyboard: 키보드에 특수 문자가 아닌 다른 값이 입력 되었을 때 호출됩니다. 입력된 값이 _input에 넘겨집니다.
+ */
 int seat_practice_input_keyboard(char _input)
 {
     if (progress < 100 && output_buffer)
@@ -534,16 +555,16 @@ int seat_practice_input_keyboard(char _input)
 }
 
 /**
-* word_practice_input_keyboard: 키보드에 특수 문자가 아닌 다른 값이 입력 되었을 때 호출됩니다. 입력된 값이 _input에 넘겨집니다.
-*/
+ * word_practice_input_keyboard: 키보드에 특수 문자가 아닌 다른 값이 입력 되었을 때 호출됩니다. 입력된 값이 _input에 넘겨집니다.
+ */
 int word_practice_input_keyboard(char _input)
 {
     return 0;
 }
 
 /**
-* short_sentence_practice_input_keyboard: 키보드에 특수 문자가 아닌 다른 값이 입력 되었을 때 호출됩니다. 입력된 값이 _input에 넘겨집니다.
-*/
+ * short_sentence_practice_input_keyboard: 키보드에 특수 문자가 아닌 다른 값이 입력 되었을 때 호출됩니다. 입력된 값이 _input에 넘겨집니다.
+ */
 int short_sentence_practice_input_keyboard(char _input)
 {
     if (progress == 100)
@@ -573,8 +594,8 @@ int short_sentence_practice_input_keyboard(char _input)
 }
 
 /**
-* long_sentence_practice_input_keyboard: 키보드에 특수 문자가 아닌 다른 값이 입력 되었을 때 호출됩니다. 입력된 값이 _input에 넘겨집니다.
-*/
+ * long_sentence_practice_input_keyboard: 키보드에 특수 문자가 아닌 다른 값이 입력 되었을 때 호출됩니다. 입력된 값이 _input에 넘겨집니다.
+ */
 int long_sentence_practice_input_keyboard(char _input)
 {
     if (progress == 100)
@@ -601,8 +622,8 @@ int long_sentence_practice_input_keyboard(char _input)
 }
 
 /**
-* menu_input_keyboard_enter: 키보드에 Enter키 또는 줄바꿈 문자가 입력 되었을 때 호출됩니다.
-*/
+ * menu_input_keyboard_enter: 키보드에 Enter키 또는 줄바꿈 문자가 입력 되었을 때 호출됩니다.
+ */
 int menu_input_keyboard_enter(void)
 {
     if (input_num > 0 && input_buffer[input_num - 1])
@@ -612,8 +633,8 @@ int menu_input_keyboard_enter(void)
 }
 
 /**
-* menu_input_keyboard_enter: 키보드에 Enter키 또는 줄바꿈 문자가 입력 되었을 때 호출됩니다.
-*/
+ * menu_input_keyboard_enter: 키보드에 Enter키 또는 줄바꿈 문자가 입력 되었을 때 호출됩니다.
+ */
 int seat_practice_input_keyboard_enter(void)
 {
     if (progress == 100)
@@ -623,13 +644,14 @@ int seat_practice_input_keyboard_enter(void)
 }
 
 /**
-* word_practice_input_keyboard_enter: 키보드에 Enter키 또는 줄바꿈 문자가 입력 되었을 때 호출됩니다.
-*/
+ * word_practice_input_keyboard_enter: 키보드에 Enter키 또는 줄바꿈 문자가 입력 되었을 때 호출됩니다.
+ */
 int word_practice_input_keyboard_enter(void)
 {
     if (!strncmp(input_buffer, "###", 3))
     {
         screen_change(TYPE_MENU);
+
         return 0;
     }
     else if (progress < 100 && output_buffer)
@@ -651,6 +673,7 @@ int word_practice_input_keyboard_enter(void)
             input_buffer[input_num - 1] = 0;
 
         typing_count++;
+
         accuracy = 100 - num_of_typo * 100 / typing_count;
     }
     else if (progress == 100)
@@ -660,8 +683,8 @@ int word_practice_input_keyboard_enter(void)
 }
 
 /**
-* short_sentence_practice_input_keyboard_enter: 키보드에 Enter키 또는 줄바꿈 문자가 입력 되었을 때 호출됩니다.
-*/
+ * short_sentence_practice_input_keyboard_enter: 키보드에 Enter키 또는 줄바꿈 문자가 입력 되었을 때 호출됩니다.
+ */
 int short_sentence_practice_input_keyboard_enter(void)
 {
     if (progress < 100 && output_buffer)
@@ -691,8 +714,8 @@ int short_sentence_practice_input_keyboard_enter(void)
 }
 
 /**
-* long_sentence_practice_input_keyboard_enter: 키보드에 Enter키 또는 줄바꿈 문자가 입력 되었을 때 호출됩니다.
-*/
+ * long_sentence_practice_input_keyboard_enter: 키보드에 Enter키 또는 줄바꿈 문자가 입력 되었을 때 호출됩니다.
+ */
 int long_sentence_practice_input_keyboard_enter(void)
 {
     if (progress == 100)
@@ -736,32 +759,32 @@ int long_sentence_practice_input_keyboard_enter(void)
 }
 
 /**
-* menu_input_keyboard_backspace: 키보드에 backspace키 또는 delete가 입력 되었을 때 호출됩니다.
-*/
+ * menu_input_keyboard_backspace: 키보드에 backspace키 또는 delete가 입력 되었을 때 호출됩니다.
+ */
 int menu_input_keyboard_backspace(void)
 {
     return 0;
 }
 
 /**
-* seat_practice_input_keyboard_backspace: 키보드에 backspace키 또는 delete가 입력 되었을 때 호출됩니다.
-*/
+ * seat_practice_input_keyboard_backspace: 키보드에 backspace키 또는 delete가 입력 되었을 때 호출됩니다.
+ */
 int seat_practice_input_keyboard_backspace(void)
 {
     return 0;
 }
 
 /**
-* word_practice_input_keyboard_backspace: 키보드에 backspace키 또는 delete가 입력 되었을 때 호출됩니다.
-*/
+ * word_practice_input_keyboard_backspace: 키보드에 backspace키 또는 delete가 입력 되었을 때 호출됩니다.
+ */
 int word_practice_input_keyboard_backspace(void)
 {
     return 0;
 }
 
 /**
-* short_sentence_practice_input_keyboard_backspace: 키보드에 backspace키 또는 delete가 입력 되었을 때 호출됩니다.
-*/
+ * short_sentence_practice_input_keyboard_backspace: 키보드에 backspace키 또는 delete가 입력 되었을 때 호출됩니다.
+ */
 int short_sentence_practice_input_keyboard_backspace(void)
 {
     if ((progress == 0 && input_num == 0) || progress == 100)
@@ -796,8 +819,8 @@ int short_sentence_practice_input_keyboard_backspace(void)
 }
 
 /**
-* long_sentence_practice_input_keyboard_backspace: 키보드에 backspace키 또는 delete가 입력 되었을 때 호출됩니다.
-*/
+ * long_sentence_practice_input_keyboard_backspace: 키보드에 backspace키 또는 delete가 입력 되었을 때 호출됩니다.
+ */
 int long_sentence_practice_input_keyboard_backspace(void)
 {
     if ((progress == 0 && input_num == 0) || progress == 100)
@@ -831,17 +854,21 @@ int long_sentence_practice_input_keyboard_backspace(void)
         output_buffer = output_buffer_page1;
 
         strcpy(input_buffer, tmp_input_buffer);
+
         input_max = strlen(output_buffer);
         input_num = strlen(input_buffer);
+
         progress = 0;
+
+        return 1;
     }
 
     return 0;
 }
 
 /**
-* long_sentence_divide_line: _output_buffer을 두개의 배열로 나누어 줍니다.
-*/
+ * long_sentence_divide_line: _output_buffer을 두개의 배열로 나누어 줍니다.
+ */
 int long_sentence_divide_line(char *_output_buffer, char *_output_buffer_page1, char *_output_buffer_page2)
 {
     int line = 0;
